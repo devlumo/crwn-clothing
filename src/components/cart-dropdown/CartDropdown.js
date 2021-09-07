64abe40dd41355efac78c82d4e5aeb8934328a56
Luke Molloy <developerlumo@gmail.com>
2021-09-07 15:32:18 +0100

    added CartItem component and display cart items in Cart dropdown

--- src/components/cart-dropdown/CartDropdown.js
+++ src/components/cart-dropdown/CartDropdown.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { connect } from "react-redux";
 import CustomButton from "../custom-button/CustomButton";
 import "./cart-dropdown.styles.scss";
+import CartItem from "../cart-item/CartItem";
 
-export default function CartDropdown() {
+function CartDropdown({ cartItems }) {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
 }
+
+const mapStateToProps = (state) => ({
+  cartItems: state.cart.cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
