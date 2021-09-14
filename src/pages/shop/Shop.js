32be7f0950c0673f8fc4fff9ca0ce0958633a1e7
Luke Molloy <developerlumo@gmail.com>
2021-09-14 11:01:56 +0100

    added shop and directory reducers to handle state for their components

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,24 +1,12 @@
-import React, { Component } from "react";
-import SHOP_DATA from "./shop.data";
-import CollectionPreview from "../../components/collection-preview/CollectionPreview";
+import React from "react";
+import CollectionsOverview from "../../components/collections-overview/CollectionsOverview";
 
-class Shop extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const Shop = () => {
+  return (
+    <div className="shop-page">
+      <CollectionsOverview />
+    </div>
+  );
+};
 
 export default Shop;
